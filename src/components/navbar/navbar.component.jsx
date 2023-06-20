f4c05a68672e47a4fdf1358bd30fb4ae67a81606
web-development <dward1711@gmail.com>
2023-06-19 22:24:08 -0500

    Updated navbar to Tailwind styled component

--- src/components/navbar/navbar.component.jsx
+++ src/components/navbar/navbar.component.jsx
@@ -11,21 +11,22 @@ import { FaGithub, FaLinkedinIn } from 'react-icons/fa'
 import { BsFillPersonLinesFill } from 'react-icons/bs'
 import { CustomIcon, CustomIconNav } from '../customs/custom-icon.component'
 import CustomLink from '../customs/custom-link.component'
+import { NavContainer, NavWrapper } from './navbar.styles'
 
 const Navbar = () => {
-	const [ nav, setNav]  = useState(false)
-	const [ shadow, setShadow ] = useState(false)
-	const [ navBg, setNavBg ] = useState('#ecf0f3')
-	const [ linkColor, setLinkColor ] = useState('text-gray-700')
+	const [nav, setNav] = useState(false)
+	const [shadow, setShadow] = useState(false)
+	const [navBg, setNavBg] = useState('#ecf0f3')
+	const [linkColor, setLinkColor] = useState('text-gray-700')
 	const pathname = usePathname()
 
 	useEffect(() => {
-		if(pathname === '/projects/dwfazhionz') {
+		if (pathname === '/projects/dwfazhionz') {
 			setNavBg('transparent')
 			setLinkColor('#ecf0f3')
 		} else {
 			setNavBg('#ecf0f3')
-			setLinkColor('#1f2937')
+			setLinkColor(linkColor)
 		}
 	}, [pathname])
 
@@ -35,20 +36,18 @@ const Navbar = () => {
 
 	useEffect(() => {
 		const handleShadow = () => {
-			if(window.scrollY >= 90) {
+			if (window.scrollY >= 90) {
 				setShadow(true)
 			} else {
 				setShadow(false)
 			}
-		} 
+		}
 		window.addEventListener('scroll', handleShadow)
 	}, [])
 
 	return (
-		<div 
-		style={{ backgroundColor: `${navBg}` }}
-		className={shadow ? 'fixed w-full h-20 bg-[{navBg}] shadow-xl z-[100]' : 'fixed w-full h-20 z-[100]'}>
-			<div className='flex items-center justify-between w-full h-full px-4 2xl:px-16'>
+		<NavContainer shadow={shadow} style={{ backgroundColor: `${navBg}` }}>
+			<NavWrapper>
 				{/**
 				 * ********** LOGO **********
 				 */}
@@ -57,35 +56,27 @@ const Navbar = () => {
 				{/**
 				 * ********** NAV **********
 				 */}
-				<ul style={{ color: `${linkColor}` }} className='hidden md:flex text-sm uppercase'>
+				<ul
+					style={{ color: `${linkColor}` }}
+					className='hidden md:flex text-sm uppercase'>
 					<CustomLink href='/'>
-						<li className='ml-10'>
-							Home
-						</li>
+						<li className='ml-10'>Home</li>
 					</CustomLink>
 
 					<CustomLink href='/#about'>
-						<li className='ml-10'>
-							About
-						</li>
+						<li className='ml-10'>About</li>
 					</CustomLink>
 
 					<CustomLink href='/#skills'>
-						<li className='ml-10'>
-							Skills
-						</li>
+						<li className='ml-10'>Skills</li>
 					</CustomLink>
 
 					<CustomLink href='/#projects'>
-						<li className='ml-10'>
-							Projects
-						</li>
+						<li className='ml-10'>Projects</li>
 					</CustomLink>
 
 					<CustomLink href='/#contact'>
-						<li className='ml-10'>
-							Contact Me
-						</li>
+						<li className='ml-10'>Contact Me</li>
 					</CustomLink>
 				</ul>
 
@@ -95,8 +86,7 @@ const Navbar = () => {
 					className='md:hidden hover:cursor-pointer'>
 					<AiOutlineMenu size={25} />
 				</div>
-			</div>
-
+			</NavWrapper>
 
 			{/**
 			 * **** Mobile Navigation ****
@@ -116,13 +106,11 @@ const Navbar = () => {
 							? 'fixed left-0 top-0 w-[75%] sm:w-[60%] md:w-[45%] h-screen bg-[#ecf0f3] p-8 ease-in duration-500'
 							: 'fixed left-[-100%] top-0 p-5 ease-in duration-500'
 					}>
-
-
 					{/**
 					 * **** Profile Picture ****
 					 */}
 					<div className='flex w-full items-start justify-between '>
-					 	<div className='flex-col items-center'>
+						<div className='flex-col items-center'>
 							<Image
 								src='/../public/assets/portfolio-pic.jpg'
 								width='85'
@@ -134,14 +122,13 @@ const Navbar = () => {
 							<h3 className='text-gray-700'>De Sean Ward</h3>
 						</div>
 
-
 						{/**
 						 * **** Close Icon ****
 						 */}
 						<CustomIconNav>
-						<span onClick={handleNav}>
-							<AiOutlineClose />
-						</span>
+							<span onClick={handleNav}>
+								<AiOutlineClose />
+							</span>
 						</CustomIconNav>
 					</div>
 
@@ -151,30 +138,49 @@ const Navbar = () => {
 						</p>
 					</div>
 
-
 					{/**
 					 * **** Navigation Links *****
 					 */}
 					<div className='py-4 flex flex-col'>
 						<ul className='uppercase text-sm'>
 							<CustomLink href='/'>
-								<li className='pb-4' onClick={() => setNav(false)}>Home</li>
+								<li
+									className='pb-4'
+									onClick={() => setNav(false)}>
+									Home
+								</li>
 							</CustomLink>
 
 							<CustomLink href='/#about'>
-								<li className='pb-4' onClick={() => setNav(false)}>About</li>
+								<li
+									className='pb-4'
+									onClick={() => setNav(false)}>
+									About
+								</li>
 							</CustomLink>
 
 							<CustomLink href='/#skills'>
-								<li className='pb-4' onClick={() => setNav(false)}>Skills</li>
+								<li
+									className='pb-4'
+									onClick={() => setNav(false)}>
+									Skills
+								</li>
 							</CustomLink>
 
 							<CustomLink href='/#projects'>
-								<li className='pb-4' onClick={() => setNav(false)}>Projects</li>
+								<li
+									className='pb-4'
+									onClick={() => setNav(false)}>
+									Projects
+								</li>
 							</CustomLink>
 
 							<CustomLink href='/#contact'>
-								<li className='pb-4' onClick={() => setNav(false)}>Contact</li>
+								<li
+									className='pb-4'
+									onClick={() => setNav(false)}>
+									Contact
+								</li>
 							</CustomLink>
 						</ul>
 
@@ -182,7 +188,6 @@ const Navbar = () => {
 							<p className='uppercase tracking-widest text-[]'>
 								Let&#039;s Connect
 							</p>
-
 
 							{/**
 							 * **** Social Media Icons ****
@@ -208,7 +213,7 @@ const Navbar = () => {
 					</div>
 				</div>
 			</div>
-		</div>
+		</NavContainer>
 	)
 }
 
--- src/components/navbar/navbar.styles.jsx
+++ src/components/navbar/navbar.styles.jsx
@@ -1,5 +1,18 @@
 'use client'
 import styled from 'styled-components';
+import tw from 'tailwind-styled-components';
 
-export const NavContainer = styled.div`
-`;
+export const NavContainer = tw.div`
+    {
+        ${p => p.shadow 
+        ? 'fixed w-full h-20 bg-[{navBg}] shadow-xl z-[100]' 
+        : 'fixed w-full h-20 bg-[{navBg}] z-[100]'
+    }
+`
+
+export const NavWrapper = tw.div`
+    flex items-center justify-between 
+    w-full 
+    h-full 
+    px-4 2xl:px-16
+`
