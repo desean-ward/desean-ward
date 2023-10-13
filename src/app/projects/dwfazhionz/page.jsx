1f43c8ddcac4226941582c5d543485743d66a38d
De Sean Ward <d.ward.chi@gmail.com>
2023-10-13 16:47:08 -0500

    Updated URL for DW Fazhionz

--- src/app/projects/dwfazhionz/page.jsx
+++ src/app/projects/dwfazhionz/page.jsx
@@ -60,7 +60,7 @@ const dwfazhionz = () => {
 					 * **** BUTTONS ****
 					
 					 */}
-          <Link href='https://dw-fazhionz.netlify.app/' target={"_blank"}>
+          <Link href='https://dw-fazhionz.vercel.app/' target={"_blank"}>
             <button className='px-8 py-2 mt-4 mr-8'>Demo</button>
           </Link>
 
