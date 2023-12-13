59dc9b938917d5dc0e72b7ca7c3ef5cbf0e079e3
De Sean Ward <d.ward.chi@gmail.com>
2023-12-13 09:41:39 -0600

    Minor update

--- src/components/navbar/navbar.component.jsx
+++ src/components/navbar/navbar.component.jsx
@@ -1,234 +1,225 @@
-'use client';
-import React, { useState, useEffect } from 'react';
+"use client";
+import React, { useState, useEffect } from "react";
 
 // NextJS Components
-import Image from 'next/image';
-import Link from 'next/link';
-import { usePathname } from 'next/navigation';
+import Image from "next/image";
+import Link from "next/link";
+import { usePathname } from "next/navigation";
 
 // Styled Components
-import { AiOutlineMenu, AiOutlineClose, AiOutlineMail } from 'react-icons/ai';
-import { FaGithub, FaLinkedinIn } from 'react-icons/fa';
-import { BsFillPersonLinesFill } from 'react-icons/bs';
-import { CustomIcon, CustomIconNav } from '../customs/custom-icon.component';
-import CustomLink from '../customs/custom-link.component';
-import { NavContainer, NavLink, NavWrapper } from './navbar.styles';
+import { AiOutlineMenu, AiOutlineClose, AiOutlineMail } from "react-icons/ai";
+import { FaGithub, FaLinkedinIn } from "react-icons/fa";
+import { BsFillPersonLinesFill } from "react-icons/bs";
+import { CustomIcon, CustomIconNav } from "../customs/custom-icon.component";
+import CustomLink from "../customs/custom-link.component";
+import { NavContainer, NavLink, NavWrapper } from "./navbar.styles";
 
 const Navbar = () => {
-	const [nav, setNav] = useState(false);
-	const [navShadow, setNavShadow] = useState('false');
-	const [navBg, setNavBg] = useState('#ecf0f3');
-	const [linkColor, setLinkColor] = useState('text-gray-700');
-	const pathname = usePathname();
-	useEffect(() => {
-		if (pathname.match('/projects/')) {
-			setNavBg('transparent');
-			setLinkColor('#ecf0f3');
-		} else {
-			setNavBg('#ecf0f3');
-			setLinkColor('#000000');
-		}
-	}, [pathname]);
+  const [nav, setNav] = useState(false);
+  const [navShadow, setNavShadow] = useState("false");
+  const [navBg, setNavBg] = useState("#ecf0f3");
+  const [linkColor, setLinkColor] = useState("text-gray-700");
+  const pathname = usePathname();
+  useEffect(() => {
+    if (pathname.match("/projects/")) {
+      setNavBg("transparent");
+      setLinkColor("#ecf0f3");
+    } else {
+      setNavBg("#ecf0f3");
+      setLinkColor("#000000");
+    }
+  }, [pathname]);
 
-	const handleNav = () => {
-		setNav(!nav);
-	};
+  const handleNav = () => {
+    setNav(!nav);
+  };
 
-	useEffect(() => {
-		const handleShadow = () => {
-			if (window.scrollY >= 90) {
-				setNavShadow('true');
-			} else {
-				setNavShadow('false');
-			}
-		};
-		window.addEventListener('scroll', handleShadow);
+  useEffect(() => {
+    const handleShadow = () => {
+      if (window.scrollY >= 90) {
+        setNavShadow("true");
+      } else {
+        setNavShadow("false");
+      }
+    };
+    window.addEventListener("scroll", handleShadow);
 
-		return () => window.removeEventListener('scroll', handleShadow);
-	}, []);
+    return () => window.removeEventListener("scroll", handleShadow);
+  }, []);
 
-	return (
-		<NavContainer
-			doshadow={navShadow}
-			style={{ backgroundColor: `${navBg}` }}>
-			<NavWrapper>
-				{/**
-				 * ********** LOGO **********
-				 */}
-				<h2 style={{ color: `${linkColor}` }}>De Sean Ward</h2>
+  return (
+    <NavContainer doshadow={navShadow} style={{ backgroundColor: `${navBg}` }}>
+      <NavWrapper>
+        {/**
+         * ********** LOGO **********
+         */}
+        <CustomLink href='/'>
+          <h2 style={{ color: `${linkColor}` }}>De Sean Ward</h2>
+        </CustomLink>
 
-				{/**
-				 * ********** NAV **********
-				 */}
-				<ul
-					style={{ color: `${linkColor}` }}
-					className='hidden md:flex text-sm uppercase'>
-					<CustomLink href='/'>
-						<li className='ml-10'>Home</li>
-					</CustomLink>
+        {/**
+         * ********** NAV **********
+         */}
+        <ul
+          style={{ color: `${linkColor}` }}
+          className='hidden text-sm uppercase md:flex'
+        >
+          <CustomLink href='/'>
+            <li className='ml-10'>Home</li>
+          </CustomLink>
 
-					<CustomLink href='/about'>
-						<li className='ml-10'>About</li>
-					</CustomLink>
+          <CustomLink href='/about'>
+            <li className='ml-10'>About</li>
+          </CustomLink>
 
-					<CustomLink href='/skills'>
-						<li className='ml-10'>Skills</li>
-					</CustomLink>
+          <CustomLink href='/skills'>
+            <li className='ml-10'>Skills</li>
+          </CustomLink>
 
-					<CustomLink href='/projects'>
-						<li className='ml-10'>Projects</li>
-					</CustomLink>
+          <CustomLink href='/projects'>
+            <li className='ml-10'>Projects</li>
+          </CustomLink>
 
-					<CustomLink href='/contact'>
-						<li className='ml-10'>Contact Me</li>
-					</CustomLink>
-				</ul>
+          <CustomLink href='/contact'>
+            <li className='ml-10'>Contact Me</li>
+          </CustomLink>
+        </ul>
 
-				<div
-					onClick={handleNav}
-					style={{ color: `${linkColor}` }}
-					className='md:hidden hover:cursor-pointer'>
-					<AiOutlineMenu size={25} />
-				</div>
-			</NavWrapper>
+        <div
+          onClick={handleNav}
+          style={{ color: `${linkColor}` }}
+          className='md:hidden hover:cursor-pointer'
+        >
+          <AiOutlineMenu size={25} />
+        </div>
+      </NavWrapper>
 
-			{/**
-			 * **** Mobile Navigation ****
-			 */}
-			<div
-				className={
-					nav
-						? 'md:hidden fixed left-0 top-0 w-full h-screen bg-black/70'
-						: ' '
-				}>
-				{/**
-				 * **** Mobile Navigation Content ****
-				 */}
-				<div
-					className={
-						nav
-							? 'fixed left-0 top-0 w-[75%] sm:w-[60%] md:w-[45%] h-screen bg-[#ecf0f3] p-8 ease-in duration-500'
-							: 'fixed left-[-100%] top-0 p-5 ease-in duration-500'
-					}>
-					{/**
-					 * **** Profile Picture ****
-					 */}
-					<div className='flex w-full items-start justify-between '>
-						<div className='flex-col items-center'>
-							<Image
-								src='/../public/assets/portfolio-pic.jpg'
-								width='85'
-								height='30'
-								className='rounded-full'
-								alt='/'
-							/>
+      {/**
+       * **** Mobile Navigation ****
+       */}
+      <div
+        className={
+          nav ? "md:hidden fixed left-0 top-0 w-full h-screen bg-black/70" : " "
+        }
+      >
+        {/**
+         * **** Mobile Navigation Content ****
+         */}
+        <div
+          className={
+            nav
+              ? "fixed left-0 top-0 w-[75%] sm:w-[60%] md:w-[45%] h-screen bg-[#ecf0f3] p-8 ease-in duration-500"
+              : "fixed left-[-100%] top-0 p-5 ease-in duration-500"
+          }
+        >
+          {/**
+           * **** Profile Picture ****
+           */}
+          <div className='flex items-start justify-between w-full '>
+            <div className='flex-col items-center'>
+              <Image
+                src='/../public/assets/portfolio-pic.jpg'
+                width='85'
+                height='30'
+                className='rounded-full'
+                alt='/'
+              />
 
-							<h3 className='text-gray-700'>De Sean Ward</h3>
-						</div>
+              <h3 className='text-gray-700'>De Sean Ward</h3>
+            </div>
 
-						{/**
-						 * **** Close Icon ****
-						 */}
-						<CustomIconNav>
-							<span onClick={handleNav}>
-								<AiOutlineClose />
-							</span>
-						</CustomIconNav>
-					</div>
+            {/**
+             * **** Close Icon ****
+             */}
+            <CustomIconNav>
+              <span onClick={handleNav}>
+                <AiOutlineClose />
+              </span>
+            </CustomIconNav>
+          </div>
 
-					<div className='border-b border-gray-300 my-4'>
-						<p className='w-[85%] md:w-[90%] py-4'>
-							Let&#039;s build solutions!
-						</p>
-					</div>
+          <div className='my-4 border-b border-gray-300'>
+            <p className='w-[85%] md:w-[90%] py-4'>
+              Let&#039;s build solutions!
+            </p>
+          </div>
 
-					{/**
-					 * **** Navigation Links *****
-					 */}
-					<div className='py-4 flex flex-col'>
-						<ul className='uppercase text-sm'>
-							<CustomLink href='/' as='/home' >
-								<li
-									className='pb-4'
-									onClick={() => setNav(false)}>
-									Home
-								</li>
-							</CustomLink>
+          {/**
+           * **** Navigation Links *****
+           */}
+          <div className='flex flex-col py-4'>
+            <ul className='text-sm uppercase'>
+              <CustomLink href='/' as='/home'>
+                <li className='pb-4' onClick={() => setNav(false)}>
+                  Home
+                </li>
+              </CustomLink>
 
-							<CustomLink href='/about'>
-								<li
-									className='pb-4'
-									onClick={() => setNav(false)}>
-									About
-								</li>
-							</CustomLink>
+              <CustomLink href='/about'>
+                <li className='pb-4' onClick={() => setNav(false)}>
+                  About
+                </li>
+              </CustomLink>
 
-							<CustomLink href='/skills'>
-								<li
-									className='pb-4'
-									onClick={() => setNav(false)}>
-									Skills
-								</li>
-							</CustomLink>
+              <CustomLink href='/skills'>
+                <li className='pb-4' onClick={() => setNav(false)}>
+                  Skills
+                </li>
+              </CustomLink>
 
-							<CustomLink href='/projects'>
-								<li
-									className='pb-4'
-									onClick={() => setNav(false)}>
-									Projects
-								</li>
-							</CustomLink>
+              <CustomLink href='/projects'>
+                <li className='pb-4' onClick={() => setNav(false)}>
+                  Projects
+                </li>
+              </CustomLink>
 
-							<CustomLink href='/contact'>
-								<li
-									className='pb-4'
-									onClick={() => setNav(false)}>
-									Contact
-								</li>
-							</CustomLink>
-						</ul>
+              <CustomLink href='/contact'>
+                <li className='pb-4' onClick={() => setNav(false)}>
+                  Contact
+                </li>
+              </CustomLink>
+            </ul>
 
-						<div className='pt-40'>
-							<p className='uppercase tracking-widest text-[]'>
-								Let&#039;s Connect
-							</p>
+            <div className='pt-40'>
+              <p className='uppercase tracking-widest text-[]'>
+                Let&#039;s Connect
+              </p>
 
-							{/**
-							 * **** Social Media Icons ****
-							 */}
-							<div className='flex items-center justify-between my-4 w-full sm:w-[80%]'>
-								<Link
-									href='https://www.linkedin.com/in/desean-ward'
-									target='_blank'>
-									<CustomIconNav>
-										<FaLinkedinIn />
-									</CustomIconNav>
-								</Link>
+              {/**
+               * **** Social Media Icons ****
+               */}
+              <div className='flex items-center justify-between my-4 w-full sm:w-[80%]'>
+                <Link
+                  href='https://www.linkedin.com/in/desean-ward'
+                  target='_blank'
+                >
+                  <CustomIconNav>
+                    <FaLinkedinIn />
+                  </CustomIconNav>
+                </Link>
 
-								<Link
-									href='https://www.github.com/desean-ward'
-									target='_blank'>
-									<CustomIconNav>
-										<FaGithub />
-									</CustomIconNav>
-								</Link>
+                <Link href='https://www.github.com/desean-ward' target='_blank'>
+                  <CustomIconNav>
+                    <FaGithub />
+                  </CustomIconNav>
+                </Link>
 
-								<Link href='/contact'>
-									<CustomIconNav>
-										<AiOutlineMail />
-									</CustomIconNav>
-								</Link>
+                <Link href='/contact'>
+                  <CustomIconNav>
+                    <AiOutlineMail />
+                  </CustomIconNav>
+                </Link>
 
-								<CustomIconNav>
-									<BsFillPersonLinesFill />
-								</CustomIconNav>
-							</div>
-						</div>
-					</div>
-				</div>
-			</div>
-		</NavContainer>
-	);
+                <CustomIconNav>
+                  <BsFillPersonLinesFill />
+                </CustomIconNav>
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
+    </NavContainer>
+  );
 };
 
 export default Navbar;
--- src/components/projects/projects.component.jsx
+++ src/components/projects/projects.component.jsx
@@ -1,86 +1,83 @@
-import React from 'react';
+import React from "react";
 
-import ProjectItem from '../project-item/project-item.component';
+import ProjectItem from "../project-item/project-item.component";
 
 import {
-	ProjectsContainer,
-	ProjectsContentWrapper,
-	ProjectsHeader,
-	ProjectsWrapper,
-} from './projects.styles';
+  ProjectsContainer,
+  ProjectsContentWrapper,
+  ProjectsHeader,
+  ProjectsWrapper,
+} from "./projects.styles";
 
-import dwFazhionz from '../../../public/assets/projects/dw-fazhionz.jpg';
-import luxeLiving from '../../../public/assets/projects/luxe-living.jpg';
+import dwFazhionz from "../../../public/assets/projects/dw-fazhionz.jpg";
+import luxeLiving from "../../../public/assets/projects/luxe-living.jpg";
 
-import comingsoon from '../../../public/assets/projects/coming-soon.png';
+import comingsoon from "../../../public/assets/projects/coming-soon.png";
 
-import { motion } from 'framer-motion';
+import { motion } from "framer-motion";
 
 const Projects = () => {
-	const slideDown = {
-		initial: { y: '-100', opacity: 0 },
-		animate: { y: 0, opacity: 1, transition: { delay: 1, duration: 0.5 } },
-	};
+  const slideDown = {
+    initial: { y: "-100", opacity: 0 },
+    animate: { y: 0, opacity: 1, transition: { delay: 1, duration: 0.5 } },
+  };
 
-	const slideUp = {
-		initial: { y: 100, opacity: 0 },
-		animate: {
-			y: 0,
-			opacity: 1,
-			transition: { delay: 1.25, duration: 0.5 },
-		},
-	};
+  const slideUp = {
+    initial: { y: 100, opacity: 0 },
+    animate: {
+      y: 0,
+      opacity: 1,
+      transition: { delay: 1.25, duration: 0.5 },
+    },
+  };
 
-	return (
-		<ProjectsContainer id='projects' className=''>
-		<ProjectsHeader
-			variants={slideUp}
-			initial='initial'
-			animate='animate'>
-			<p className='section-name'>Projects</p>
-			<h2 className='section-title'>What I&#039;ve Built</h2>
-		</ProjectsHeader>
-			<ProjectsContentWrapper>
+  return (
+    <ProjectsContainer id='projects' className=''>
+      <ProjectsHeader variants={slideUp} initial='initial' animate='animate'>
+        <p className='section-name'>Projects</p>
+        <h2 className='section-title'>What I&#039;ve Built</h2>
+      </ProjectsHeader>
+      <ProjectsContentWrapper>
+        <ProjectsWrapper
+          variants={slideDown}
+          initial='initial'
+          animate='animate'
+        >
+          {/**
+           ****  Add the "ready='true'"" property to 'ProjectItem' components to enable the 'More Info' overlay ****
+           */}
+          {/**
+           * *** D.W. FAZHIONZ ***
+           */}
+          <ProjectItem
+            ready='true'
+            img={dwFazhionz}
+            title='D.W. Fazhionz!'
+            text='A mock fashion e-Commerce website built with React and Firebase.'
+            page='/projects/dwfazhionz'
+          />
 
-				<ProjectsWrapper
-					variants={slideDown}
-					initial='initial'
-					animate='animate'>
-					{/**
-					 ****  Add the "ready='true'"" property to 'ProjectItem' components to enable the 'More Info' overlay ****
-					 */}
-					{/**
-					 * *** D.W. FAZHIONZ ***
-					 */}
-					<ProjectItem
-						ready='true'
-						img={dwFazhionz}
-						title='D.W. Fazhionz!'
-						text='A fashion e-Commerce website built with React and Firebase.'
-						page='/projects/dwfazhionz'
-					/>
-					
-					<ProjectItem
-						ready='true'
-						img={luxeLiving}
-						title='LuxeLiving Estates'
-						text='A real estate website built with the MERN stack.'
-						page='/projects/luxeLiving'
-					/>
+          <ProjectItem
+            ready='true'
+            img={luxeLiving}
+            title='LuxeLiving Estates'
+            text='A mock real estate website built with the MERN stack.'
+            page='/projects/luxeLiving'
+          />
 
-					{/**
-					 * *** D.W. FAZHIONZ ***
-					 */}
-					<ProjectItem
-						img={comingsoon}
-						title='Stay Tuned'
-						text='Coming Soon'
-						page='/projects'
-					/>
-				</ProjectsWrapper>
-			</ProjectsContentWrapper>
-		</ProjectsContainer>
-	);
+          {/**
+           * *** D.W. FAZHIONZ ***
+           */}
+          <ProjectItem
+            img={comingsoon}
+            title='Stay Tuned'
+            text='Coming Soon'
+            page='/projects'
+          />
+        </ProjectsWrapper>
+      </ProjectsContentWrapper>
+    </ProjectsContainer>
+  );
 };
 
 export default Projects;
